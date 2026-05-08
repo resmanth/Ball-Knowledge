b1a995eac5cc3dac6af39d44eccb9c697737a7f6
resmanth <resmanth2007@gmail.com>
2026-05-08 13:58:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,21 @@ import SquadBuilder from './views/SquadBuilder';
 
 function App() {
   return (
-    // BrowserRouter is the main wrapper that enables routing in our React app
+    
     <BrowserRouter>
       <div className="app-container">
-        {/* The Navbar will always be visible on every page */}
+       
         <Navbar />
         
         <main className="main-content">
-          {/* The Routes component looks at the current URL and decides which Route to render */}
+         
           <Routes>
-            {/* When the URL is exactly "/", it shows the Home component */}
+            
             <Route path="/" element={<Home />} />
             
-            {/* When the URL is "/teams", it shows the Teams component */}
+            
             <Route path="/teams" element={<Teams />} />
             
-            {/* The ":id" part is a dynamic parameter. It allows us to go to "/teams/4819" and grab the ID inside the Teams component */}
             <Route path="/teams/:id" element={<Teams />} />
             
             <Route path="/players" element={<Players />} />
